77c17a57a83a4f84104767486429e3ff876844cc
piglig <zhu1197437384@gmail.com>
2024-03-15 17:39:40 +0800

    feat: card's drop zone

--- Assets/Scripts/DragDrop.cs
+++ Assets/Scripts/DragDrop.cs
@@ -5,20 +5,53 @@ using UnityEngine;
 public class DragDrop : MonoBehaviour
 {
     private bool isDragging = false;
-    // Start is called before the first frame update
+    private GameObject startParent;
+    private Vector2 startPosition;
+
+    private GameObject dropZone;
+    private bool isOverDropZone;
+
+    public GameObject DropZone;
+    public GameObject Canvas;
+    
     void Start()
     {
-        
+        Canvas = GameObject.Find("Main Canvas");
+        DropZone = GameObject.Find("DropZone");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Colliding!");
+        isOverDropZone = true;
+        dropZone = collision.gameObject;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        Debug.Log("Uncolliding!");
+        isOverDropZone = false;
+        dropZone = null;
     }
 
     public void StartDrag()
     {
         isDragging = true;
+        startParent = transform.parent.gameObject;
+        startPosition = transform.position;
     }
 
     public void EndDrag()
     {
         isDragging = false;
+        if (isOverDropZone)
+        {
+            transform.SetParent(dropZone.transform, false);
+        } else
+        {
+            transform.position = startPosition;
+            transform.SetParent(startParent.transform, false);
+        }
     }
 
     // Update is called once per frame
@@ -27,6 +60,7 @@ public class DragDrop : MonoBehaviour
         if (isDragging) 
         {
             transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            transform.SetParent(Canvas.transform, true);
         }
     }
 }

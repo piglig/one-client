8ff4d2919ceb1f45e6fd4e4f5a16d1460f67ee4f
piglig <zhu1197437384@gmail.com>
2024-03-16 14:51:03 +0800

    feat: Connect with each other Player

--- Assets/Scripts/DrawCards.cs
+++ Assets/Scripts/DrawCards.cs
@@ -1,31 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
-public class DrawCards : MonoBehaviour
+public class DrawCards : NetworkBehaviour
 {
-    public GameObject Card1;
-    public GameObject Card2;
 
-    public GameObject PlayerArea;
+    public PlayerManager PlayerManager;
 
     public void OnClick()
     {
-        for (int i = 0; i < 5; i++)
-        {
-            GameObject card = Instantiate(Card1, new Vector2(0, 0), Quaternion.identity);
-            card.transform.SetParent(PlayerArea.transform, false);
-        }
-    }
-
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        NetworkIdentity identity = NetworkClient.connection.identity;
+        PlayerManager = identity.GetComponent<PlayerManager>();
+        PlayerManager.CmdDealCards();
     }
 }
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+
+
+public class PlayerManager : NetworkBehaviour 
+{
+    public GameObject Card1;
+    public GameObject Card2;
+    public GameObject PlayerArea;
+    public GameObject EnemyArea;
+    public GameObject DropZone;
+
+    List<GameObject> cards = new List<GameObject>();
+    
+    public override void OnStartClient() {
+        base.OnStartClient();
+
+        PlayerArea = GameObject.Find("PlayerArea");
+        EnemyArea = GameObject.Find("EnemyArea");
+        DropZone = GameObject.Find("DropZone");
+    }
+
+    [Server]
+    public override void OnStartServer() {
+        base.OnStartServer();
+
+        cards.Add(Card1);
+        cards.Add(Card2);
+        Debug.Log(cards);
+    }
+
+    [Command]
+    public void CmdDealCards()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            GameObject card = Instantiate(cards[Random.Range(0, cards.Count)], new Vector2(0, 0), Quaternion.identity);
+            NetworkServer.Spawn(card, connectionToClient);
+            card.transform.SetParent(PlayerArea.transform, false);
+            RpcShowCard(card, "Dealt");
+        }
+    }
+
+    [ClientRpc]
+    void RpcShowCard(GameObject card, string type) {
+        if (type == "Dealt") {
+            if (isOwned) {
+                card.transform.SetParent(PlayerArea.transform, false);
+            } else {
+                card.transform.SetParent(EnemyArea.transform, false);
+            }
+        } else if (type == "Played") {
+
+        }
+    }
+}
